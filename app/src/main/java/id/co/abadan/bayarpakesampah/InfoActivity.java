a34d0db0734c687d97a1a637e447dc21b4e71dd8
nugi <suprayoga.edi@gmail.com>
2018-05-07 12:42:25 +0700

    master

--- app/src/main/java/id/co/abadan/bayarpakesampah/InfoActivity.java
+++ app/src/main/java/id/co/abadan/bayarpakesampah/InfoActivity.java
@@ -1,13 +1,37 @@
 package id.co.abadan.bayarpakesampah;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class InfoActivity extends AppCompatActivity {
+    private Button btnBack, btnLogout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+
+        btnBack = (Button) findViewById(R.id.back_btn);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(InfoActivity.this, MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                startActivity(intent);
+            }
+        });
+
+        btnLogout = (Button) findViewById(R.id.logout_btn);
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(InfoActivity.this, LoginActivity.class);
+                finish();
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/id/co/abadan/bayarpakesampah/SaldoActivity.java
+++ app/src/main/java/id/co/abadan/bayarpakesampah/SaldoActivity.java
@@ -1,13 +1,37 @@
 package id.co.abadan.bayarpakesampah;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SaldoActivity extends AppCompatActivity {
+    private Button btnBack, btnLogout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_saldo);
+
+        btnBack = (Button) findViewById(R.id.back_btn);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SaldoActivity.this, MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                startActivity(intent);
+            }
+        });
+
+        btnLogout = (Button) findViewById(R.id.logout_btn);
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SaldoActivity.this, LoginActivity.class);
+                finish();
+                startActivity(intent);
+            }
+        });
     }
 }
