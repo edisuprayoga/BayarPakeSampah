43061a3f80d8e812468da12f13f46ac8756d6bf4
nugi <suprayoga.edi@gmail.com>
2018-05-08 22:59:52 +0700

    master

--- app/src/main/java/id/co/abadan/bayarpakesampah/FragmentSetting.java
+++ app/src/main/java/id/co/abadan/bayarpakesampah/FragmentSetting.java
@@ -1,12 +1,16 @@
 package id.co.abadan.bayarpakesampah;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class FragmentSetting extends Fragment {
+    Activity context;
 
     public FragmentSetting() {
 
@@ -19,7 +23,21 @@ public class FragmentSetting extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        context = getActivity();
         return inflater.inflate(R.layout.fragment_setting, container, false);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Button btnLogout = (Button) context.findViewById(R.id.logout_btn);
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 }
